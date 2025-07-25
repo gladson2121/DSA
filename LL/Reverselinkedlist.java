a56b778f05b345f5135fcf788582ad41df46b383
gladsn <gladsonbernard.d@gmail.com>
2025-07-25 12:23:02 +0530

    Added LL folder for linked list

---
+++ LL/Reverselinkedlist.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=null;
+        ListNode next=null;
+
+        curr = head;
+
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev =  curr;
+            curr = next;
+
+        }
+
+        return prev;
+        
+    }
+}
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Input: head = [1,2]
+// Output: [2,1]
